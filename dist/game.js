f38b6a34800a51291ff4ecce004c5f3feb78daea
Keith Reynolds <keithreynoldsworld@gmail.com>
2015-05-12 21:48:46 -0500

    iAmAnIdiot

--- dist/game.js
+++ dist/game.js
@@ -1,7 +1,15 @@
-function* chooseYourAdventure() {
+/*function* chooseYourAdventure() {
 	console.log(yield adventure.say('Hey there!'));
 	console.log(yield adventure.say('Woah', 'This is cool'));
 	console.log(yield adventure.ask('Woah', 'This is cool'));
 	console.log(yield adventure.choose('Woah', 'This is cool', 'Hello world'));
 	console.log(yield adventure.say('That\'s all folks!', 'THE END'));
-}
+}*/
+
+
+
+chooseYourAdventure(yield adventure.say('Hey there!'));
+
+
+
+
