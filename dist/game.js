8e022f7ef5cf36f65bb5628583c1567023b36853
Keith Reynolds <keithreynoldsworld@gmail.com>
2015-05-13 22:20:01 -0500

    itreallyworksnowIswear

--- dist/game.js
+++ dist/game.js
@@ -7,7 +7,7 @@ function* chooseYourAdventure(){
 	var yourAge = yield adventure.ask('What is your age?');
 
 	
-	if(yourAge < 15 ){yield adventure.say(yourName + 'you are way too young to be thinking of such things.','Maybe you should go hang out at http://www.pbskids.org');}
+	if(yourAge < 15 ){yield adventure.say(yourName + ', you are way too young to be thinking of such things.','Maybe you should go hang out at http://www.pbskids.org');}
 
 	else if(yourAge > 27){yield adventure.say('YOU ARE BORING. If you were really a rock star you\'d be dead by now.','Maybe you should consider being a Customer Service Representative. THE END');}
 
@@ -15,13 +15,17 @@ function* chooseYourAdventure(){
 
 	var whichDrug = yield adventure.choose('Which of the following drugs do you do on a regular basis?','Marijuana', 'Molly', 'Cocaine', 'All of the Above');
 
-	if(whichDrug === ('Marijuana' || 'Molly' || 'Cocaine')){yield adventure.say('YOU ARE BORING. rock stars do all the drugs they can.','Perhaps you could consider being a high school guidance counselor. THE END');}
+	if(whichDrug === 'Marijuana'){yield adventure.say('YOU ARE BORING. rock stars do all the drugs they can.','Perhaps you could consider being a high school guidance counselor. THE END');}
+	else if(whichDrug === 'Molly'){yield adventure.say('YOU ARE BORING. rock stars do all the drugs they can.','Perhaps you could consider being a high school guidance counselor. THE END');}
+	else if(whichDrug === 'Cocaine'){yield adventure.say('YOU ARE BORING. rock stars do all the drugs they can.','Perhaps you could consider being a high school guidance counselor. THE END');}
 
 	else {yield adventure.say('users are losers except for ROCK STARS! nice job.','please continue');} 
 
 	var whichActivity = yield adventure.choose('Which activity would you prefer on a Saturday Night?','Save a kitten', 'Read a true crime novel', 'Study French', 'Get drunk and drown in a pool');
 
-	if(whichActivity === ('Save a kitten' || 'Read a true crime novel' || 'Study French')){yield adventure.say('YOU ARE BORING.','perhaps you should consider being a veterinarian, a detective, or a mime. THE END');} 
+	if(whichActivity === 'Save a kitten'){yield adventure.say('YOU ARE BORING.','perhaps you should consider being a veterinarian, a detective, or a mime. THE END');} 
+	else if(whichActivity === 'Read a true crime novel'){yield adventure.say('YOU ARE BORING.','perhaps you should consider being a veterinarian, a detective, or a mime. THE END');} 
+	else if(whichActivity === 'Study French'){yield adventure.say('YOU ARE BORING.','perhaps you should consider being a veterinarian, a detective, or a mime. THE END');} 
 
 	else {yield adventure.say('WOW YOU REALLY ARE A ROCK STAR AT HEART!!!','congratulations, we\'ll have fun listening to your songs while you rest in peace. THE END.');} 
 				
